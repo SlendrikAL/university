003a5cba411a86c87c21fc09401c96615ad59ecb
Slendrik <103889984+SlendrikAL@users.noreply.github.com>
2024-12-01 23:39:47 +0300

    Update 1.py

--- lab6/1.py
+++ lab6/1.py
@@ -1,1 +1,19 @@
+class UserAccount:
+    username = None
+    email = None
+    password = None
+    def __init__(self,username,email,password):
+        self.username = username
+        self.email = email
+        self.password = password
+    def set_password(self,new_password):
+        self.password=new_password
+    def check_password(self,password):
+        if self.password == password:
+            print(True)
+        else:
+            print(False)
+UserAccount1 = UserAccount('AAAA','aaa@email.com','aaa')
 
+UserAccount1.set_password('bbb')
+UserAccount1.check_password('bbb')
