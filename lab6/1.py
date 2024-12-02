d3ce11b51658e899af4b5cbe0f8f017649150b08
Slendrik <103889984+SlendrikAL@users.noreply.github.com>
2024-12-02 21:56:33 +0300

    Update 1.py

--- lab6/1.py
+++ lab6/1.py
@@ -6,11 +6,8 @@ class UserAccount:
     def set_password(self,new_password):
         self.password=new_password
     def check_password(self,password):
-        if self.password == password:
-            print(True)
-        else:
-            print(False)
+        return self.password == password
 UserAccount1 = UserAccount('AAAA','aaa@email.com','aaa')
 
 UserAccount1.set_password('bbb')
-UserAccount1.check_password('bbb')
+print(UserAccount1.check_password('bbb'))
