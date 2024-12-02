a37781e3347abb3af1f6417b4347da7807cb90c2
Slendrik <103889984+SlendrikAL@users.noreply.github.com>
2024-12-02 20:03:21 +0300

    Update 1.py

--- lab6/1.py
+++ lab6/1.py
@@ -1,7 +1,4 @@
 class UserAccount:
-    username = None
-    email = None
-    password = None
     def __init__(self,username,email,password):
         self.username = username
         self.email = email
