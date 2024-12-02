f6c1bed5f461200a2ad0158e75cb4b763243cc5d
Slendrik <103889984+SlendrikAL@users.noreply.github.com>
2024-12-02 21:56:44 +0300

    Update 2.py

--- lab6/2.py
+++ lab6/2.py
@@ -1,17 +1,16 @@
 class Vehicle:
-    def __init__(self, make, model):
+    def __init__(self,make,model):
         self.make = make
         self.model = model
     def get_info(self):
-        print(self.make,self.model)
+        return (self.make,self.model)
 class Car(Vehicle):
+    def __init__(self,make,model, fuel_type):
+        self.fuel_type = fuel_type
         super().__init__(make,model)
-        fuel_type = None
     def get_info(self):
-        print(self.make,self.model,self.fuel_type)
-Car1 = Car()
-Car1.make = 'Honda'
-Car1.model = 'CR-V'
-Car1.fuel_type = 'Дизель'
-
-Car1.get_info()
+        return (self.make,self.model,self.fuel_type)
+Car1 = Car('Honda', 'C9', 'Дизель')
+Car2 = Car('Hyundai','Solaris','Бензин')
+print(Car1.get_info())
+print(Car2.get_info())
