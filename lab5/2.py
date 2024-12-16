d1081d24526b1e22c595e7ad1a0ac4882e226262
Slendrik <103889984+SlendrikAL@users.noreply.github.com>
2024-12-16 09:20:22 +0300

    Update 2.py

--- lab5/2.py
+++ lab5/2.py
@@ -7,17 +7,17 @@ class Circle:
         self.radius = radius
 
     def get_radius(self):
-        print(self.radius)
+        return (self.radius)
 
     def set_radius(self,new_radius):
         self.radius = new_radius
 
 Circle1 = Circle(100)
-Circle1.get_radius()
+print(Circle1.get_radius())
 
 time.sleep(3)
 
 Circle1.set_radius(156)
-Circle1.get_radius()
+print(Circle1.get_radius())
 
 
